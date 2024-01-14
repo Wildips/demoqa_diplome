f6d53ec6f158bb033d07ac784e9c9f5f028a6f74
Vitaliy Zakharov <zak125@mail.ru>
2024-01-15 01:42:54 +0300

    added root test package

--- demoqa_diplome_tests/utils/resource.py
+++ demoqa_diplome_tests/utils/resource.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def image_path(file_name):
     return str(
-        Path(__file__).parent.parent.joinpath(f"images/examples/{file_name}").absolute()
+        Path(__file__).parent.parent.parent.joinpath(f"images/examples/{file_name}").absolute()
     )
 
 
